b069cf9c5799d78f80f5a802c25e8abd6ffa90d6
Him188 <Him188@mamoe.net>
2024-11-20 13:59:51 +0000

    Use bangumi-data as schedule data source

--- protocol/src/AnimeSchedule.kt
+++ protocol/src/AnimeSchedule.kt
@@ -34,11 +34,11 @@ data class AnimeRecurrence(
 )
 
 @Serializable
-enum class AnimeSeason(val quarterNumber: Int) {
-    SPRING(1), // 1
-    SUMMER(2), // 4
-    AUTUMN(3), // 7
-    WINTER(4), // 10
+enum class AnimeSeason(val quarterNumber: Int, val monthRange: Set<Int>) {
+    WINTER(1, setOf(12, 1, 2)), // 1
+    SPRING(2, setOf(3, 4, 5)), // 4
+    SUMMER(3, setOf(6, 7, 8)), // 7
+    AUTUMN(4, setOf(9, 10, 11)), // 10
     ;
 
     companion object {
@@ -54,6 +54,14 @@ data class AnimeSeasonId(
     // serialized
     val id: String = "${year}q${season.quarterNumber}"
 
+    val yearMonthRanges
+        get() = when (season) {
+            AnimeSeason.WINTER -> listOf(year - 1 to 12, year to 11, year to 10)
+            AnimeSeason.SPRING -> listOf(year to 3, year to 4, year to 5)
+            AnimeSeason.SUMMER -> listOf(year to 6, year to 7, year to 8)
+            AnimeSeason.AUTUMN -> listOf(year to 9, year to 10, year to 11)
+        }
+
     companion object {
         private val COMPARATOR = compareBy<AnimeSeasonId> { it.year }
             .thenBy { it.season }
@@ -68,6 +76,30 @@ data class AnimeSeasonId(
                     string.substringAfter('q').toIntOrNull() ?: return null,
                 ) ?: return null,
             )
+        }
+
+        private val monthLookUpTable = arrayOfNulls<AnimeSeason>(13).apply {
+            for (season in AnimeSeason.entries) {
+                for (month in season.monthRange) {
+                    this[month] = season
+                }
+            }
+        }
+
+        fun fromDate(year: Int, month: Int): AnimeSeasonId {
+            if (month == 12) {
+                return AnimeSeasonId(year + 1, AnimeSeason.WINTER)
+            }
+            require(month in 1..12) { "Invalid month: $month" }
+            return AnimeSeasonId(year, monthLookUpTable[month]!!)
+//            return when (month) {
+//                1, 2 -> AnimeSeasonId(year, AnimeSeason.WINTER)
+//                12 -> AnimeSeasonId(year + 1, AnimeSeason.WINTER)
+//                3, 4, 5 -> AnimeSeasonId(year, AnimeSeason.SPRING)
+//                6, 7, 8 -> AnimeSeasonId(year, AnimeSeason.SUMMER)
+//                9, 10, 11 -> AnimeSeasonId(year, AnimeSeason.AUTUMN)
+//                else -> throw IllegalArgumentException("Invalid month: $month")
+//            }
         }
     }
 
--- server/src/service/AnimeScheduleService.kt
+++ server/src/service/AnimeScheduleService.kt
@@ -1,21 +1,28 @@
 package me.him188.ani.danmaku.server.service
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.plugins.ResponseException
 import io.ktor.client.plugins.UserAgent
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.accept
 import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsChannel
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
+import io.ktor.utils.io.jvm.javaio.toInputStream
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
 import me.him188.ani.danmaku.protocol.AnimeRecurrence
 import me.him188.ani.danmaku.protocol.AnimeSchedule
 import me.him188.ani.danmaku.protocol.AnimeSeason
@@ -24,12 +31,13 @@ import me.him188.ani.danmaku.protocol.OnAirAnimeInfo
 import me.him188.ani.danmaku.server.util.ConcurrentMemoryCache
 import me.him188.ani.danmaku.server.util.ConcurrentMemoryCacheMap
 import me.him188.ani.danmaku.server.util.getOrPut
+import me.him188.ani.danmaku.server.util.query.json
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
 
 class AnimeScheduleService(
-    private val fetcher: AnimeScheduleFetcher = BgmlistAnimeScheduleFetcher,
+    private val fetcher: AnimeScheduleFetcher = BangumiDataAnimeScheduleFetcher,
 ) {
     private val seasonsCache = ConcurrentMemoryCache<List<AnimeSeasonId>>(60.minutes)
     private val scheduleCache = ConcurrentMemoryCacheMap<AnimeSeasonId, AnimeSchedule>(60.minutes)
@@ -57,8 +65,8 @@ interface AnimeScheduleFetcher {
     suspend fun fetchSchedule(season: AnimeSeasonId): AnimeSchedule?
 }
 
-// https://bgmlist.com/archive/2024q4
-object BgmlistAnimeScheduleFetcher : AnimeScheduleFetcher {
+//https://github.com/bangumi-data/bangumi-data
+object BangumiDataAnimeScheduleFetcher : AnimeScheduleFetcher {
     private val httpClient by lazy {
         HttpClient {
             install(ContentNegotiation) {
@@ -77,30 +85,33 @@ object BgmlistAnimeScheduleFetcher : AnimeScheduleFetcher {
         }
     }
 
-    override suspend fun fetchSeasonIds(): List<AnimeSeasonId> {
-        val resp = httpClient.get("https://bgmlist.com/_next/data/AvTmOI7jFk3EFuTLj3Ooa/archive.json") {
-            accept(ContentType.Application.Json)
-        }.body<BgmlistPagedResponse<BgmlistArchive>>()
-        return resp.pageProps.seasons.mapNotNull {
-            parseToAnimeYearSeasonOrNull(it)
+    private suspend fun <T> HttpResponse.bodyDeserialized(deserializationStrategy: DeserializationStrategy<T>): T {
+        return bodyAsChannel().let { channel ->
+            channel.toInputStream().use {
+                json.decodeFromStream(deserializationStrategy, it)
+            }
         }
     }
 
-    override suspend fun fetchSchedule(season: AnimeSeasonId): AnimeSchedule? {
-        val seasonStr = "${season.year}q${season.season.quarterNumber}"
-        val resp = try {
-            httpClient.get("https://bgmlist.com/_next/data/AvTmOI7jFk3EFuTLj3Ooa/archive/${seasonStr}.json?season=${seasonStr}") {
+    override suspend fun fetchSeasonIds(): List<AnimeSeasonId> {
+        val resp =
+            httpClient.get("https://raw.githubusercontent.com/bangumi-data/bangumi-data/refs/heads/master/dist/data.json") {
                 accept(ContentType.Application.Json)
-            }.body<BgmlistPagedResponse<BgmlistAnimeSchedule>>()
-        } catch (e: ResponseException) {
-            if (e.response.status == HttpStatusCode.NotFound || e.response.status == HttpStatusCode.InternalServerError) {
-                return null
-            }
-            throw e
+            }.bodyDeserialized(RawBangumiData.serializer())
+
+        return resp.items.mapTo(HashSet()) {
+            val localDateTime = Instant.parse(it.begin).toLocalDateTime(TimeZone.of("Asia/Tokyo"))
+            AnimeSeasonId.fromDate(localDateTime.year, localDateTime.monthNumber)
+        }.toList()
+    }
+
+    override suspend fun fetchSchedule(season: AnimeSeasonId): AnimeSchedule {
+        val resp = season.yearMonthRanges.flatMap { (year, month) ->
+            getMonthDataOrNull(year, month) ?: emptyList()
         }
 
         return AnimeSchedule(
-            list = resp.pageProps.items.mapNotNull { anime ->
+            list = resp.mapNotNull { anime ->
                 OnAirAnimeInfo(
                     bangumiId = anime.sites.find { it.site == "bangumi" }?.id?.toIntOrNull() ?: return@mapNotNull null,
                     name = anime.title.takeIf { it.isNotEmpty() } ?: return@mapNotNull null,
@@ -112,6 +123,24 @@ object BgmlistAnimeScheduleFetcher : AnimeScheduleFetcher {
                 )
             },
         )
+    }
+
+    private suspend fun getMonthDataOrNull(year: Int, month: Int): List<BangumiDataAnime>? {
+        try {
+            return httpClient.get(
+                "https://raw.githubusercontent.com/bangumi-data/bangumi-data/refs/heads/master/data/items" +
+                        "/$year" +
+                        "/${month.toString().padStart(2, '0')}" +
+                        ".json",
+            ) {
+                accept(ContentType.Application.Json)
+            }.bodyDeserialized(ListSerializer(BangumiDataAnime.serializer()))
+        } catch (e: ResponseException) {
+            if (e.response.status == HttpStatusCode.NotFound || e.response.status == HttpStatusCode.InternalServerError) {
+                return null
+            }
+            throw e
+        }
     }
 
     private fun parseToInstant(value: String): Instant? {
@@ -146,37 +175,41 @@ object BgmlistAnimeScheduleFetcher : AnimeScheduleFetcher {
 }
 
 @Serializable
-private data class BgmlistPagedResponse<T>(
-    val pageProps: T
-)
-
-@Serializable
-private data class BgmlistArchive(
-    val seasons: List<String>,
-)
-
-@Serializable
-private data class BgmlistAnimeSchedule(
-    val items: List<BgmlistAnime>
-)
-
-@Serializable
-private data class BgmlistAnime(
+private data class BangumiDataAnime(
     val title: String,
     val titleTranslate: Map<String, List<String>> = emptyMap(),
     val begin: String = "",
     val broadcast: String = "",
     val end: String = "",
-    val sites: List<BgmlistSite>
+    val sites: List<BangumiDataAnimeSite>
+)
+
+/**
+ * [BangumiDataAnime] 的只包含 [begin] 的版本.
+ */
+@Serializable
+private data class BangumiDataAnimeBeginOnly(
+    val begin: String = "",
 )
 
 @Serializable
-private data class BgmlistSite(
+private data class BangumiDataAnimeSite(
     val site: String = "", // name
     val id: String = "",
 )
 
 suspend fun main() {
-//    println(AnimeScheduleService().getSeasons())
+//    println(AnimeScheduleService().getSeasonIds())
     println(AnimeScheduleService().getAnimeSchedule(AnimeSeasonId(2024, AnimeSeason.WINTER)))
 }
+
+///////////////////////////////////////////////////////////////////////////
+// Raw
+///////////////////////////////////////////////////////////////////////////
+
+@Serializable
+private data class RawBangumiData(
+    val items: List<
+            BangumiDataAnimeBeginOnly,
+            >
+)
