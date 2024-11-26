cadd9af8ff6513540611653737cefdb5136e2b99
StageGuard <sg@mamoe.net>
2024-11-26 16:26:03 +0800

    get lain url of subject image.

--- server/src/service/BangumiTrendsService.kt
+++ server/src/service/BangumiTrendsService.kt
@@ -3,10 +3,22 @@ package me.him188.ani.danmaku.server.service
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
+import io.ktor.client.request.request
 import io.ktor.client.statement.bodyAsChannel
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.userAgent
 import io.ktor.utils.io.jvm.javaio.toInputStream
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.sync.withPermit
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import me.him188.ani.danmaku.protocol.TrendingSubject
 import me.him188.ani.danmaku.protocol.Trends
@@ -23,6 +35,7 @@ class BangumiTrendsService {
                     ignoreUnknownKeys = true
                 }
             }
+            followRedirects = false
         }
     }
 
@@ -35,6 +48,7 @@ class BangumiTrendsService {
     suspend fun getTrends(): Trends = cache.getOrPut {
         lock.withLock {
             cache.get()?.let { return it }
+            
 
             val subjects = httpClient
                 .get("https://bangumi.tv/anime/browser/?sort=trends")
@@ -47,15 +61,43 @@ class BangumiTrendsService {
                     val id = element.attr("href").substringAfterLast("/").toIntOrNull()
                         ?: return@mapNotNull null
 
-                    TrendingSubject(
-                        bangumiId = id,
-                        nameCn = element.text(),
-                        imageLarge = "https://api.bgm.tv/v0/subjects/${id}/image?type=large",
-                    )
+                    SubjectMetadata(id = id, nameCn = element.text())
                 }
+            
+            val result = coroutineScope { 
+                val semaphore = Semaphore(8)
+                subjects.map { s ->
+                    async { 
+                        val lainImageUrl = semaphore.withPermit { getImageLainUrl(s.id) }
+                        TrendingSubject(
+                            bangumiId = s.id,
+                            nameCn = s.nameCn,
+                            imageLarge = lainImageUrl ?: "https://api.bgm.tv/v0/subjects/${s.id}/image?type=large",
+                        )
+                    }
+                }
+            }
 
-            Trends(subjects)
+            Trends(awaitAll(*result.toTypedArray()))
         }
+    }
+    
+    private suspend fun getImageLainUrl(subjectId: Int): String? {
+        val resp = withContext(Dispatchers.IO) {
+            httpClient.get("https://api.bgm.tv/v0/subjects/$subjectId/image?type=large") {
+                userAgent(UA)
+            }
+        }
+        if (resp.status == HttpStatusCode.Found) {
+            return resp.headers[HttpHeaders.Location]
+        }
+        return null
+    }
+    
+    private data class SubjectMetadata(val id: Int, val nameCn: String)
+    
+    companion object {
+        private const val UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
     }
 }
 
