a7789f39f94d62e2f18b02964d647058257f6f9a
Him188 <Him188@mamoe.net>
2024-12-02 15:53:14 +0000

    Fix SubscriptionWhitelistModel

--- server/src/data/model/SubscriptionWhitelistModel.kt
+++ server/src/data/model/SubscriptionWhitelistModel.kt
@@ -3,7 +3,7 @@ package me.him188.ani.danmaku.server.data.model
 import org.bson.codecs.pojo.annotations.BsonId
 import java.util.UUID
 
-class SubscriptionWhitelistModel(
+data class SubscriptionWhitelistModel(
     @BsonId
     val id: String = UUID.randomUUID().toString(),
     val url: String,
